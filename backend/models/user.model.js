03da17aad5035772e0f879b5456f250016c29ef2
Danny Field <58800432+DannyField@users.noreply.github.com>
2020-06-11 00:17:31 +1000

    Added routes

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,7 @@
-import { Schema, Mongoose } from "mongoose";
+// import { Schema, Mongoose } from "mongoose";
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
@@ -6,7 +9,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
+      //   This will trim white space off
       trim: true,
+
+      //   Username must be at least 3 characters long
       minlength: 3,
     },
   },
