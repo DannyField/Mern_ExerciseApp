53ce30e9d9e1ce22e145f35c18460c5f4d0830bd
Danny Field <58800432+DannyField@users.noreply.github.com>
2020-06-12 14:04:07 +1000

    finished backend code

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 // Bringing in the mongoose exercise model
 let User = require("../models/exercise.model");
 
-// Our first endpoint, that gets GET requests
+// Our first endpoint, that gets GET requests, handles HTML get requests
 router.route("/").get((req, res) => {
   //   The find() will look for all the users in the mongoose database and returns a promise
   Exercise.find()
@@ -10,7 +10,7 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-// Gets POST requests
+// Gets POST requests. If it is a post request
 router.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
@@ -27,6 +27,35 @@ router.route("/add").post((req, res) => {
   newExercise
     .save()
     .then(() => res.json("Exercise added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// router. GET the id for the exercise
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise has been Updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
